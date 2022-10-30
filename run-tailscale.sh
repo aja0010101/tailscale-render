4524f7aa66715c5060d2928838170e860425ec2a
aja0010101 <61653782+aja0010101@users.noreply.github.com>
2022-10-30 11:56:17 +0800

    Update run-tailscale.sh

--- run-tailscale.sh
+++ run-tailscale.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 /render/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-PID=$!
+#PID=$!
 
-ADVERTISE_ROUTES=${ADVERTISE_ROUTES:-10.0.0.0/8}
+#ADVERTISE_ROUTES=${ADVERTISE_ROUTES:-10.0.0.0/8}
 #until /render/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="${RENDER_SERVICE_NAME}" --advertise-routes="$ADVERTISE_ROUTES" --advertise-exit-node; do
 until /render/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="${RENDER_SERVICE_NAME}" --advertise-exit-node; do
   sleep 0.1
 done
 export ALL_PROXY=socks5://localhost:1055/
 tailscale_ip=$(/render/tailscale ip)
+sleep infinity
 echo "Tailscale is up at IP ${tailscale_ip}"
 
-wait ${PID}
+#wait ${PID}
